c5a5c8f764b826d6d0247a22800501623f529945
Clifford Myrtil <cliffordhamyrtil@gmail.com>
2023-08-25 21:06:40 -0400

    Renamed cs file

---
+++ Variables_Exercise1/Variables Exercise CS File.cs
@@ -0,0 +1,14 @@
+﻿//Declaring and Initialize Variables
+string name = "Johnathan Price";
+int age = 31;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 69.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
